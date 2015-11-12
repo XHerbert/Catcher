e97417b3fa3c1623749938499b6e2ff05cd93d57
xhb <421271727@qq.com>
2015-11-12 21:43:11 +0800

    Add random color
    Add random color for labels

--- Catcher/TasksForm.cs
+++ Catcher/TasksForm.cs
@@ -11,6 +11,7 @@ namespace Catcher
 {
     public partial class TasksForm : Catcher.FormMaster
     {
+        private const int iSeed = 10;
         public TasksForm()
         {
             InitializeComponent();
@@ -29,7 +30,10 @@ namespace Catcher
             }
             foreach (var task in existTasks)
             {
-                LabelWithCheck lbck = BuildLabel(task.Name);
+                long tick = DateTime.Now.Ticks;
+                Random ran = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+                int seed= ran.Next(0, 9);
+                LabelWithCheck lbck = BuildLabel(task.Name,seed);
                 lbck.Location = new System.Drawing.Point(13+(88 * m), 20+(32*k));
                 URLTasks.Controls.Add(lbck);
                 i++;
@@ -54,15 +58,30 @@ namespace Catcher
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
-        private LabelWithCheck BuildLabel(string name)
+        private LabelWithCheck BuildLabel(string name,int color)
         {
             LabelWithCheck lbck = new LabelWithCheck();
             lbck.LabelText = name;
-            lbck.BackColor = System.Drawing.Color.LightPink;
-            lbck.ForeColor = System.Drawing.Color.Black;
+            lbck.BackColor = Color.LightPink;
+            lbck.ForeColor = Color.Black;
+            lbck.BackColor = ColorTranslator.FromHtml(ColorConvertor(color));
             lbck.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             lbck.Size = new System.Drawing.Size(74, 22);
             return lbck;
+        }
+
+        //private Color MakeColor(int color)
+        //{
+        //    //int colorHex = (DataModel.EnumValuePack.ColorTable)color;
+        //    string colorResult="#"+Enum.GetName(typeof(EnumValuePack), color);
+        //    return ColorTranslator.FromHtml(colorResult);
+        //}
+
+        private static string ColorConvertor(int flag)
+        {
+            StringBuilder builder = new StringBuilder("#");
+            builder.Append(((EnumValuePack.ColorTable)flag).ToString());
+            return builder.ToString();
         }
     }
 }
---
+++ DataModel/EnumValuePack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class EnumValuePack
+    {
+        public enum ColorTable
+        {
+            FFCCFF,
+            FF00FF,
+            CCCC00,
+            CC0000,
+            FF6600,
+            CCFF99,
+            CCCCCC,
+            CC9900,
+            FF6666,
+            FFFF99
+        }
+    }
+}
